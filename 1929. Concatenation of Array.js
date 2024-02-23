836119cd68f5be08148da8c7ff28fc1b180f68f8
syedniaz <snm051110@gmail.com>
2024-02-23 18:31:37 +0600

    1929. Concatenation of Array

---
+++ 1929. Concatenation of Array.js
@@ -0,0 +1,4 @@
+var getConcatenation = function(nums) {
+    let new_nums = [...nums, ...nums];
+    return new_nums;
+};
---
+++ 1929. Concatenation of Array.py
@@ -0,0 +1,4 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        new_nums = nums + nums
+        return new_nums
