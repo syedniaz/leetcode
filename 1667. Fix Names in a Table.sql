c8c0846065227c964249b54710cb8e2a31e3da48
syedniaz <snm051110@gmail.com>
2024-09-16 22:33:36 +0600

    1667. Fix Names in a Table

---
+++ 1667. Fix Names in a Table.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select user_id, CONCAT(
+        UPPER(SUBSTRING(name, 1, 1)),
+        LOWER(SUBSTRING(name, 2, LENGTH(name)))
+    ) AS name
+From Users
+order by user_id
